729db005e8d3c030ea505b130375bfa7503e0032
abduallahB <abduallah.imu2@gmail.com>
2021-05-04 00:20:36 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const imgDiv = document.getElementById('dog-image-container')
+const select = document.getElementById('breed-dropdown')
+
+
+
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then((res) => {
+    return res.json();
+  })
+  .then((json) => {
+    console.log(json);
+  })
+
+
+ select.addEventListener('click',function(e){
+    const imgs = []
+     imgs.document.createElement('img')
+    
+
+ }) ;
+  
+
+
